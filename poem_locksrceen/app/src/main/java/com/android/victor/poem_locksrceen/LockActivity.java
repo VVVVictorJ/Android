13b4fc2782b1d1839dbcead56cd16fd8968dba6b
GREEK_VICTOR <PastoreMaxwell@outlook.com>
2018-12-19 13:27:20 +0800

    lock project
    Poem Lock Project

--- poem_locksrceen/app/src/main/java/com/android/victor/poem_locksrceen/LockActivity.java
+++ poem_locksrceen/app/src/main/java/com/android/victor/poem_locksrceen/LockActivity.java
@@ -1,9 +1,14 @@
 package com.android.victor.poem_locksrceen;
 
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LockActivity extends AppCompatActivity {
+
+    private DevicePolicyManager policyManager;
+    private ComponentName   componentName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
