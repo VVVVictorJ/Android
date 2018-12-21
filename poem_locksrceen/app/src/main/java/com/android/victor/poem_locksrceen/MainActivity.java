f0b173a7b12246dab1c3f0761ae89f03858cf799
GREEK_VICTOR <PastoreMaxwell@outlook.com>
2018-12-21 16:12:55 +0800

    new
    new

---
+++ poem_locksrceen/app/src/main/java/com/android/victor/poem_locksrceen/MainActivity.java
@@ -0,0 +1,16 @@
+package com.android.victor.poem_locksrceen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, LockService.class);
+        startService(intent);
+    }
+}
